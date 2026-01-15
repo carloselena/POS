42fecb0dad7d1dcae6ae20743db38166eef5c1d6
carloselena <carloselenadev@gmail.com>
2026-01-15 14:36:52 -0400

    PersistenceInfrastructure registered

---
+++ POS.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace POS.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
--- POS/Program.cs
+++ POS/Program.cs
@@ -1,4 +1,5 @@
 using POS.Core.Application;
+using POS.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddApplicationLayer();
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

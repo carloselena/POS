07a370a2fb99555226c86fbe8bcadab675b4435f
carloselena <carloselenadev@gmail.com>
2026-01-15 15:12:29 -0400

    GenericRepository created and registered with its interface

---
+++ POS.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Core.Application.Interfaces.Repositories;
+using POS.Infrastructure.Persistence.Contexts;
+
+namespace POS.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<Entity> : IGenericRepository<Entity>
+        where Entity : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Entity> AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            return await Task.FromResult(entity);
+        }
+
+        public async Task<Entity> UpdateAsync(Entity entity)
+        {
+            _dbContext.Update(entity);
+            return await Task.FromResult(entity);
+        }
+
+        public Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<Entity>> GetAllAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public async Task<Entity?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+
+        public async Task<int> GetTotalRows()
+        {
+            return await _dbContext.Set<Entity>().CountAsync();
+        }
+    }
+}
--- POS.Infrastructure.Persistence/ServiceRegistration.cs
+++ POS.Infrastructure.Persistence/ServiceRegistration.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using POS.Core.Application.Interfaces.Repositories;
 using POS.Infrastructure.Persistence.Contexts;
+using POS.Infrastructure.Persistence.Repositories;
 
 namespace POS.Infrastructure.Persistence
 {
@@ -14,6 +16,10 @@ namespace POS.Infrastructure.Persistence
                                                         options.UseSqlServer(connectionString,
                                                         m => m.MigrationsAssembly(typeof(ApplicationDbContext)
                                                         .Assembly.FullName)));
+
+            #region Repositories
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            #endregion
         }
     }
 }

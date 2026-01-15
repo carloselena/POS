240a4dd6c795c545adb46697e959db418b9dcc86
carloselena <carloselenadev@gmail.com>
2026-01-15 19:14:10 -0400

    CreateMeassurementUnitCommand created

---
+++ POS.Core.Application/Features/MeassurementUnits/Commands/CreateMeassurementUnit/CreateMeassurementUnitCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace POS.Core.Application.Features.MeassurementUnits.Commands.CreateMeassurementUnit
+{
+    public class CreateMeassurementUnitCommand : IRequest<int>
+    {
+        public required string Name { get; set; }
+        public required string Abbreviation { get; set; }
+    }
+}

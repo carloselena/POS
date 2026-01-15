423a43060ff3660fbc0427ffdb186f8ccdf8804b
carloselena <carloselenadev@gmail.com>
2026-01-15 15:51:34 -0400

    PaginationFilter and PagedResult<T> DTOs created

---
+++ POS.Core.Application/Common/DTOs/PagedResult.cs
@@ -0,0 +1,8 @@
+﻿namespace POS.Core.Application.Common.DTOs
+{
+    public class PagedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; set; } = [];
+        public int TotalCount { get; set; }
+    }
+}
---
+++ POS.Core.Application/Common/DTOs/PaginationFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace POS.Core.Application.Common.DTOs
+{
+    public class PaginationFilter
+    {
+        public int Page { get; set; }
+        public int RecordsPerPage { get; set; }
+    }
+}

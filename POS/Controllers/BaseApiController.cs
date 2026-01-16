ffae1aac0f8db32dafcb87b0260ad3f1c8c95f37
carloselena <carloselenadev@gmail.com>
2026-01-15 20:30:49 -0400

    BaseApiController created using IMediator

---
+++ POS/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POS.WebApi.Controllers
+{
+    public abstract class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}

66614fc77eeb3eb203c2bf17bfb09f52bd21b270
Darlin04 <darlinnieve47@gmail.com>
2026-01-17 17:35:58 -0400

    Correccion de errores en el dominio de category

--- POS.Core.Domain/Entities/Category.cs
+++ POS.Core.Domain/Entities/Category.cs
@@ -1,50 +1,56 @@
-﻿namespace POS.Core.Domain.Entities
+﻿using POS.Core.Domain.Exceptions;
+using POS.Core.Domain.Exceptions.BusinessRuleException;
+
+namespace POS.Core.Domain.Entities
 {
+    
+
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal ITBIS { get; set; }
 
-        private readonly List<Product> Products => _products.AsReadOnly();
+        private readonly List<Product> _products = new List<Product>();
+
 
         private  Category (){}
 
-        private Category(string  Name, decimal itbis){
+        private Category(string  name, decimal itbis){
 
             if(string.IsNullOrWhiteSpace(name)){
                 throw new BusinessRuleException("El nombre de categoria es obligatorio");
             }   
 
-            if(name.length < 2 || name.Length >100){
-                throw new BusinessRuleExeption("El nombre debe tener entre 2 y 100 caracteres.");
+            if(name.Length < 2 || name.Length >10){
+                throw new BusinessRuleException("El nombre debe tener entre 2 y 10 caracteres.");
             }
 
             if(itbis < 0 || itbis > 1){
-                throw new BusinessRuleExeption("El ITBIS debe estar entre 0% y 100%");
+                throw new BusinessRuleException("El ITBIS debe estar entre 0% y 100%");
             }
 
             Name = name.Trim();
-            ITBIS = itbis
+            ITBIS = itbis;
         }
 
         public static Category Create(string name, decimal itbis){
             return new Category(name, itbis);
         }
 
-        public void changeName(string newName){
+        public void ChangeName(string newName){
             if(string.IsNullOrWhiteSpace(newName)){
                 throw new BusinessRuleException("El nombre de la categoria es obligatorio");
             }
 
-            if(newName.length< 2 || newName.length > 10){
-                throw new BusinessRuleException("El nombre debe tener entre 2 y 100 caracteres maximo")
+            if(newName.Length< 2 || newName.Length > 10){
+                throw new BusinessRuleException("El nombre debe tener entre 2 y 100 caracteres maximo");
             }
 
-            Name = newName.trim();
+            Name = newName.Trim();
         }
 
-        public void chageItbis(decimal newItbis){
+        public void ChangeItbis(decimal newItbis){
             if(newItbis < 0 || newItbis > 1){
                 throw new BusinessRuleException("El ITBIS debe ser mayor a 0% y menor a 100%");
             }
@@ -54,3 +60,6 @@
          
     }
 }
+
+
+

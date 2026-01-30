8fee9a49d8a9980a86eb7d386c3d8cdf026c565f
carloselena <carloselenadev@gmail.com>
2026-01-30 16:14:28 -0400

    MeasurementUnit to Dto mapping created

---
+++ POS.Core.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using POS.Core.Application.Features.MeassurementUnits.Queries;
+using POS.Core.Domain.Entities;
+
+namespace POS.Core.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            #region MeasurementUnit
+            CreateMap<MeasurementUnit, MeasurementUnitDto>();
+            #endregion
+        }
+    }
+}

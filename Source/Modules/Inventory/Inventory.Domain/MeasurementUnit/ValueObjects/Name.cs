6637ab7d1f37ea0d54a59b5098bb64a303e9c6df
carloselena <carloselenadev@gmail.com>
2026-02-04 21:54:05 -0400

    MeasurementUnitName value object renamed and moved to internal folder

---
+++ Source/Modules/Inventory/Inventory.Domain/MeasurementUnit/ValueObjects/Name.cs
@@ -0,0 +1,13 @@
+﻿using Core.Shared.Guards;
+using Core.Shared.ValueObjects;
+
+namespace Inventory.Domain.MeasurementUnit.ValueObjects;
+
+public sealed record Name : StringValueObject
+{
+    public Name(string value) : base(value)
+    {
+        Guard.AgainstNullOrWhiteSpace(value, nameof(Name));
+        Guard.AgainstMaxLength(value, 20, nameof(Name));
+    }
+}

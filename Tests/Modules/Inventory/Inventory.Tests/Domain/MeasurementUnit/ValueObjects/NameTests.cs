7ff98f5251fa2a256765c30a56f2e3bc82882329
carloselena <carloselenadev@gmail.com>
2026-02-15 14:35:54 -0400

    Created all missing tests for MeasurementUnit Name value object

--- Tests/Modules/Inventory/Inventory.Tests/Domain/MeasurementUnit/ValueObjects/NameTests.cs
+++ Tests/Modules/Inventory/Inventory.Tests/Domain/MeasurementUnit/ValueObjects/NameTests.cs
@@ -11,4 +11,40 @@ public class NameTests
         var name = new Name(value);
         Assert.That(name.Value, Is.EqualTo(value));
     }
+
+    [Test]
+    public void ShouldThrowWhenValueIsNull()
+    {
+        Assert.Throws<ArgumentException>(() => new Name(null!));
+    }
+
+    [Test]
+    public void ShouldThrowWhenValueIsEmpty()
+    {
+        Assert.Throws<ArgumentException>(() => new Name(string.Empty));
+    }
+    
+    [Test]
+    public void ShouldThrowWhenValueIsWhiteSpace()
+    {
+        Assert.Throws<ArgumentException>(() => new Name("   "));
+    }
+    
+    [Test]
+    public void ShouldThrowWhenValueExceedsMaxLength()
+    {
+        var value = new string('A', 21);
+
+        Assert.Throws<ArgumentException>(() => new Name(value));
+    }
+    
+    [Test]
+    public void ShouldAllowValueWithExactly20Characters()
+    {
+        var value = new string('A', 20);
+
+        var name = new Name(value);
+
+        Assert.That(name.Value, Is.EqualTo(value));
+    }
 }

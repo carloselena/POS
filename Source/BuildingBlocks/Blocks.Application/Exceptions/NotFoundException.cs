8bb13123023ba689dbbbbf9486b952a94b3fe490
carloselena <carloselenadev@gmail.com>
2026-02-17 18:58:18 -0400

    Blocks.Application with Validation and NotFound exceptions, and ValidationBehavior created

---
+++ Source/BuildingBlocks/Blocks.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Blocks.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() : base("Recurso no encontrado")
+    {
+    }
+
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
---
+++ Source/BuildingBlocks/Blocks.EntityFramework/RepositoryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Blocks.Application.Exceptions;
+using Blocks.Domain.Abstractions;
+using Blocks.Domain.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blocks.EntityFramework;
+
+public static class RepositoryExtensions
+{
+    public static async Task<TEntity> FindByIdOrThrowAsync<TEntity, TContext>(
+        this GenericRepository<TEntity, TContext> repository, Guid id, string propertyName)
+        where TContext : DbContext
+        where TEntity : class, IAggregateRoot
+    {
+        var entity = await repository.FindByIdAsync(id);
+        return entity ?? throw new NotFoundException($"Recurso {propertyName} no encontrado");
+    }
+    
+    public static async Task<TEntity> FindByIdOrThrowAsync<TEntity>(this DbSet<TEntity> dbSet, Guid id, string propertyName)
+        where TEntity : class, IAggregateRoot
+    {
+        var entity = await dbSet.FindAsync(id);
+        return entity ?? throw new NotFoundException($"Recurso {propertyName} no encontrado");
+    }
+    
+    public static async Task<TEntity> GetByIdOrThrowAsync<TEntity, TContext>(
+        this GenericRepository<TEntity, TContext> repository, Guid id, string propertyName)
+        where TContext : DbContext
+        where TEntity : class, IAggregateRoot
+    {
+        var entity = await repository.GetByIdAsync(id);
+        return entity ?? throw new NotFoundException($"Recurso {propertyName} no encontrado");
+    }
+}

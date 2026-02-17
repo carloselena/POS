a22216da3235cf2f5baa65e74c93c2406d8df315
carloselena <carloselenadev@gmail.com>
2026-02-17 16:19:25 -0400

    MeasurementUnitRepository implemented

--- Source/Modules/Inventory/Inventory.Persistence/Repositories/MeasurementUnitRepository.cs
+++ Source/Modules/Inventory/Inventory.Persistence/Repositories/MeasurementUnitRepository.cs
@@ -1,9 +1,18 @@
 ﻿using Inventory.Domain.MeasurementUnits;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Persistence.Repositories;
 
 public class MeasurementUnitRepository(InventoryDbContext dbContext)
-    : InventoryRepository<MeasurementUnit>(dbContext)
+    : InventoryRepository<MeasurementUnit>(dbContext), IMeasurementUnitRepository
 {
-    
+    public async Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken)
+    {
+        return await Query().AnyAsync(mu => mu.Name == name, cancellationToken);
+    }
+
+    public async Task<bool> ExistsByAbbreviationAsync(string abbreviation, CancellationToken cancellationToken = default)
+    {
+        return await Query().AnyAsync(mu => mu.Abbreviation == abbreviation, cancellationToken);
+    }
 }

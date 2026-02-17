303989643dc13b5658cb591552f128d7879460dd
carloselena <carloselenadev@gmail.com>
2026-02-17 16:19:52 -0400

    CreateMeasurementUnitCommand with validator and handler created

---
+++ Source/Modules/Inventory/Inventory.Application/Features/MeasurementUnits/Commands/CreateMeasurementUnit/CreateMeasurementUnitCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Inventory.Application.Features.MeasurementUnits.Commands.CreateMeasurementUnit;
+
+public record CreateMeasurementUnitCommand(string Name, string Abbreviation) : IRequest;

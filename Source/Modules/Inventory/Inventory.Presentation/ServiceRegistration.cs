15cde377009fe9d79955159d871695dd6cbf3cde
carloselena <carloselenadev@gmail.com>
2026-02-25 23:26:13 -0400

    Extension method created for registering the InventoryModule

--- Source/Modules/Inventory/Inventory.Presentation/ServiceRegistration.cs
+++ Source/Modules/Inventory/Inventory.Presentation/ServiceRegistration.cs
@@ -1,6 +1,15 @@
-﻿namespace Inventory.Presentation;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class ServiceRegistration
+namespace Inventory.Presentation;
+
+public static class ServiceRegistration
 {
-    
+    public static IServiceCollection AddInventoryModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        Application.ServiceRegistration.AddApplicationServices(services);
+        Persistence.ServiceRegistration.AddPersistenceServices(services, configuration);
+
+        return services;
+    }
 }

5b237b50514158ade16fb26eb7265ccfea04d2d9
carloselena <carloselenadev@gmail.com>
2026-02-26 01:57:53 -0400

    create-measurement-unit feature implemented completely

--- Source/Modules/Inventory/Inventory.Persistence/Repositories/MeasurementUnitRepository.cs
+++ Source/Modules/Inventory/Inventory.Persistence/Repositories/MeasurementUnitRepository.cs
@@ -8,11 +8,11 @@ public class MeasurementUnitRepository(InventoryDbContext dbContext)
 {
     public async Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken)
     {
-        return await Query().AnyAsync(mu => mu.MeasurementUnitName == name, cancellationToken);
+        return await Query().AnyAsync(mu => mu.MeasurementUnitName.Value == name, cancellationToken);
     }
 
     public async Task<bool> ExistsByAbbreviationAsync(string abbreviation, CancellationToken cancellationToken = default)
     {
-        return await Query().AnyAsync(mu => mu.MeasurementUnitAbbreviation == abbreviation, cancellationToken);
+        return await Query().AnyAsync(mu => mu.MeasurementUnitAbbreviation.Value == abbreviation, cancellationToken);
     }
 }

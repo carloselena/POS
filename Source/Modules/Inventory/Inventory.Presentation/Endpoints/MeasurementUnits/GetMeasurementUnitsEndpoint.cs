3d01041ec8510a485c8f17455a06c82b16d33a52
carloselena <carloselenadev@gmail.com>
2026-02-26 22:50:31 -0400

    GetMeasurementUnitsEndpoint created and registered. Feature completely working!

--- Source/Modules/Inventory/Inventory.Presentation/Endpoints/MeasurementUnits/GetMeasurementUnitsEndpoint.cs
+++ Source/Modules/Inventory/Inventory.Presentation/Endpoints/MeasurementUnits/GetMeasurementUnitsEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using Inventory.Application.Features.MeasurementUnits.Queries.GetAllMeasurementUnits;
+﻿using Inventory.Application.Features.MeasurementUnits.Queries;
+using Inventory.Application.Features.MeasurementUnits.Queries.GetAllMeasurementUnits;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -10,10 +11,14 @@ public static class GetMeasurementUnitsEndpoint
 {
     public static void MapGetMeasurementUnitsEndpoint(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/measurement-units", async (GetAllMeasurementUnitsQuery query, ISender sender) =>
+        app.MapGet("/measurement-units", async (ISender sender) =>
         {
-            var result = await sender.Send(query);
+            var result = await sender.Send(new GetAllMeasurementUnitsQuery());
             return Results.Ok(result);
-        });
+        })
+        .WithName("GetMeasurementUnits")
+        .WithTags("MeasurementUnits")
+        .Produces<List<MeasurementUnitDto>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status500InternalServerError);
     }
 }
--- Source/Modules/Inventory/Inventory.Presentation/EndpointsRegistration.cs
+++ Source/Modules/Inventory/Inventory.Presentation/EndpointsRegistration.cs
@@ -8,6 +8,7 @@ public static class EndpointsRegistration
     {
         #region MeasurementUnits
         app.MapCreateMeasurementUnitEndpoint();
+        app.MapGetMeasurementUnitsEndpoint();
         #endregion
         
         return app;

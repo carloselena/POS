9e161048b567962891b9038a7139a5a940178c1e
carloselena <carloselenadev@gmail.com>
2026-03-02 22:21:09 -0400

    Products entity created

---
+++ Source/Modules/Inventory/Inventory.Domain/Products/ValueObjects/Quantity.cs
@@ -0,0 +1,16 @@
+﻿using Blocks.Domain.Exceptions;
+using Blocks.Domain.Guards;
+
+namespace Blocks.Domain.ValueObjects;
+
+public record Quantity
+{
+    public decimal Value { get; }
+
+    public Quantity(decimal value)
+    {
+        Guard.AgainstNegativeDecimal(value, "cantidad");
+
+        Value = value;
+    }
+}
---
+++ Source/Modules/Inventory/Inventory.Domain/Products/ValueObjects/Stock.cs
@@ -0,0 +1,25 @@
+﻿using Blocks.Domain.Exceptions;
+using Blocks.Domain.Guards;
+using Blocks.Domain.ValueObjects;
+
+namespace Inventory.Domain.Products.ValueObjects;
+
+public record Stock
+{
+    public decimal Value { get; }
+
+    public Stock(decimal value = 0)
+    {
+        Guard.AgainstNegativeDecimal(value, "stock");
+        
+        Value = value;
+    }
+
+    internal Stock Increase(Quantity quantity)
+        => new Stock(Value + quantity.Value);
+
+    internal Stock Decrease(Quantity quantity)
+    {
+        return Value < quantity.Value ? throw new DomainException("Stock insuficiente") : new Stock(Value - quantity.Value);
+    }
+}

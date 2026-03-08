1e0210c5f06814be3630e7935d148359aa0690d1
carloselena <carloselenadev@gmail.com>
2026-03-08 13:42:45 -0400

    Changing positional params constructor for AutoMapper

--- Source/Modules/Inventory/Inventory.Application/Features/Products/ProductDto.cs
+++ Source/Modules/Inventory/Inventory.Application/Features/Products/ProductDto.cs
@@ -1,13 +1,16 @@
 ﻿namespace Inventory.Application.Features.Products;
 
-public record ProductDto(
-    Guid Id,
-    string BarCode,
-    string Description,
-    decimal Cost,
-    decimal Price,
-    decimal WholesaleQuantity,
-    decimal WholesalePrice,
-    decimal Stock,
-    decimal MinStock
-);
+public record ProductDto
+{
+    public Guid Id { get; init; }
+    public string BarCode { get; init; }
+    public string Description { get; init; }
+    public string Currency { get; init; }
+    public decimal Cost { get; init; }
+    public decimal Price { get; init; }
+    public decimal WholesaleQuantity { get; init; }
+    public decimal WholesalePrice { get; init; }
+    public decimal Stock { get; init; }
+    public decimal MinStock { get; init; }
+    public string MeasurementUnit { get; set; }
+}

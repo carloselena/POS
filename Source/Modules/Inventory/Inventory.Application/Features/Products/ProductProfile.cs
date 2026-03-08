cf02cb03268d3c5cbdf5c2256ba51bbb66c1ead7
carloselena <carloselenadev@gmail.com>
2026-03-08 13:43:38 -0400

    Mapping for Product to ProductDto created

--- Source/Modules/Inventory/Inventory.Application/Features/Products/ProductProfile.cs
+++ Source/Modules/Inventory/Inventory.Application/Features/Products/ProductProfile.cs
@@ -1,6 +1,19 @@
-﻿namespace Inventory.Application.Features.Products;
+﻿using AutoMapper;
+using Inventory.Domain.Products;
 
-public class ProductProfile
+namespace Inventory.Application.Features.Products;
+
+public class ProductProfile : Profile
 {
-    
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>()
+            .ForMember(dto => dto.BarCode, opt => opt.MapFrom(src => src.BarCode.Value))
+            .ForMember(dto => dto.Cost, opt => opt.MapFrom(src => src.Cost.Amount))
+            .ForMember(dto => dto.Currency, opt => opt.MapFrom(src => src.Cost.Currency))
+            .ForMember(dto => dto.Price, opt => opt.MapFrom(src => src.Price.Amount))
+            .ForMember(dto=> dto.WholesaleQuantity, opt => opt.MapFrom(src => src.WholesaleQuantity.Value))
+            .ForMember(dto => dto.WholesalePrice, opt => opt.MapFrom(src => src.WholesalePrice.Amount))
+            .ForMember(dto => dto.Stock, opt => opt.MapFrom(src => src.Stock.Value));
+    }
 }
